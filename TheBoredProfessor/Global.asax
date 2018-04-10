4a143a7b24d84252c7da2bae9688b1ffff326cff
Silveiro Gonzalez <sgoolea1993@gmail.com>
2018-04-10 14:52:54 -0500

    Add project files.

---
+++ TheBoredProfessor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheBoredProfessor.MvcApplication" Language="C#" %>
---
+++ TheBoredProfessor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBoredProfessor.Startup))]
+namespace TheBoredProfessor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
